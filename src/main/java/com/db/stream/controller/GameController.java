43d726f6ef9b1c09d9960058c6f630e0c60515d2
grui0502 <grui0992@gmail.com>
2023-02-08 23:46:58 -0500

    Library change1
    还是library的部分,刚才忘加了

--- src/main/java/com/db/stream/controller/GameController.java
+++ src/main/java/com/db/stream/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.db.stream.controller;
 import com.db.stream.entity.Game;
+import com.db.stream.entity.Comment;
 import com.db.stream.service.GameService;
 import jakarta.annotation.Resource;
 import org.springframework.data.repository.query.Param;
@@ -17,11 +18,18 @@ public class GameController {
         System.out.println("get all game name, proceeding...\n");
         return gameService.getAllGame();
     }
+    @RequestMapping("/game_show/{game_id}")
+    public List<Game> game_show(@PathVariable Integer game_id) { return gameService.getGameInfo(game_id);}
+
+    @RequestMapping("/comm_show/{game_id}")
+    public List<Comment> comm_show(@PathVariable Integer game_id) { return gameService.getGameComm(game_id);}
+
+    @RequestMapping("/numofgames")
+    public List<Game> numofgames() { return gameService.getGameNum();}
 
 //
 //    @GetMapping("/lib")
 //    public List<Game> libGameList(@RequestParam Integer u_id){
 //        return gameService.getUserGame(u_id);
 //    }
-
 }
