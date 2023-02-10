6d70260994890e5ccfcf74abe8f75173190205d3
grui0502 <grui0992@gmail.com>
2023-02-10 00:30:41 -0500

    2022.2.10
    小bug修复

--- src/main/java/com/db/stream/mapper/GameMapper.java
+++ src/main/java/com/db/stream/mapper/GameMapper.java
@@ -23,7 +23,7 @@ public interface GameMapper {
         //List<Game> getGameInfo();
     List<Comment> getGameComm(@Param("game_id") Integer game_id);
 
-    @Select("SELECT g_id FROM Game")
+    @Select("SELECT * FROM Game")
     List<Game> getGameNum();
 
 //    @Select("SELECT g.g_id, g.g_name FROM Game as g JOIN Library as L on g.g_id = L.g_id JOIN User U on U.u_id = L.u_id WHERE U.u_id == #{u_id}")
